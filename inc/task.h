d6fe2327312b4807082e993d04788d66fdae1110
arunkumarv31 <arunkumarv31@gmail.com>
2014-08-07 22:35:09 +0530

    W:Added README

--- inc/task.h
+++ inc/task.h
@@ -2,4 +2,5 @@ extern void startScheduler ( void );
 extern void initializeScheduler ( void );
 extern void changePriority ( char *name, uint8_t priority );
 extern void createTask ( void ( * function_ptr )( void ), char *taskname, uint8_t priority, uint16_t stack_size );
+extern void deleteTask ( char *name );
 extern void changeStatus ( char *name, uint8_t status );
